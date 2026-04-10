53f2aaa13f639d118100d22e43942d5bc018cd5e
Fractal Mandala <103218473+fractalmandala@users.noreply.github.com>
2026-04-10 23:00:31 +0530

    desp4

--- svelte.config.js
+++ svelte.config.js
@@ -86,7 +86,10 @@ const config = {
     ],
     extensions: ['.svelte', '.md', '.svx'],
     kit: {
-        adapter: adapter()
+        adapter: adapter(),
+        prerender: {
+            handleHttpError: 'warn'
+        }
     }
 };
 
